0c269360d5b35ca5a5036f55880fc35573a6f650
JoeyLau <csyiliu@comp.polyu.edu.hk>
2021-09-07 17:37:41 +0800

    fixed some bugs

--- exps/NAS-Bench-201-algos/GDAS.py
+++ exps/NAS-Bench-201-algos/GDAS.py
@@ -65,69 +65,72 @@ def search_func(
     print_freq,
     logger,
 ):
+    # network, criterion = torch.nn.DataParallel(search_model).cuda(), criterion.cuda()
     data_time, batch_time = AverageMeter(), AverageMeter()
     base_losses, base_top1, base_top5 = AverageMeter(), AverageMeter(), AverageMeter()
     arch_losses, arch_top1, arch_top5 = AverageMeter(), AverageMeter(), AverageMeter()
     network.train()
     end = time.time()
-    for step, (base_inputs, base_targets, arch_inputs, arch_targets) in enumerate(
-        xloader
-    ):
-        scheduler.update(None, 1.0 * step / len(xloader))
-        base_targets = base_targets.cuda(non_blocking=True)
-        arch_targets = arch_targets.cuda(non_blocking=True)
-        # measure data loading time
-        data_time.update(time.time() - end)
 
-        # update the weights
-        w_optimizer.zero_grad()
-        _, logits = network(base_inputs)
-        base_loss = criterion(logits, base_targets)
-        base_loss.backward()
-        torch.nn.utils.clip_grad_norm_(network.parameters(), 5)
-        w_optimizer.step()
-        # record
-        base_prec1, base_prec5 = obtain_accuracy(
-            logits.data, base_targets.data, topk=(1, 5)
-        )
-        base_losses.update(base_loss.item(), base_inputs.size(0))
-        base_top1.update(base_prec1.item(), base_inputs.size(0))
-        base_top5.update(base_prec5.item(), base_inputs.size(0))
+    for i in range(5):
+        for step, (base_inputs, base_targets, arch_inputs, arch_targets) in enumerate(
+            xloader
+        ):
+            scheduler.update(None, 1.0 * step / len(xloader))
+            base_targets = base_targets.cuda(non_blocking=True)
+            arch_targets = arch_targets.cuda(non_blocking=True)
+            # measure data loading time
+            data_time.update(time.time() - end)
 
-        # update the architecture-weight
-        a_optimizer.zero_grad()
-        _, logits = network(arch_inputs)
-        arch_loss = criterion(logits, arch_targets)
-        arch_loss.backward()
-        a_optimizer.step()
-        # record
-        arch_prec1, arch_prec5 = obtain_accuracy(
-            logits.data, arch_targets.data, topk=(1, 5)
-        )
-        arch_losses.update(arch_loss.item(), arch_inputs.size(0))
-        arch_top1.update(arch_prec1.item(), arch_inputs.size(0))
-        arch_top5.update(arch_prec5.item(), arch_inputs.size(0))
+            # update the weights
+            w_optimizer.zero_grad()
+            _, logits = network(base_inputs.cuda())
+            base_loss = criterion(logits, base_targets)
+            base_loss.backward()
+            torch.nn.utils.clip_grad_norm_(network.parameters(), 5)
+            w_optimizer.step()
+            # record
+            base_prec1, base_prec5 = obtain_accuracy(
+                logits.data, base_targets.data, topk=(1, 5)
+            )
+            base_losses.update(base_loss.item(), base_inputs.size(0))
+            base_top1.update(base_prec1.item(), base_inputs.size(0))
+            base_top5.update(base_prec5.item(), base_inputs.size(0))
 
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
+            # update the architecture-weight
+            a_optimizer.zero_grad()
+            _, logits = network(arch_inputs.cuda())
+            arch_loss = criterion(logits, arch_targets)
+            arch_loss.backward()
+            a_optimizer.step()
+            # record
+            arch_prec1, arch_prec5 = obtain_accuracy(
+                logits.data, arch_targets.data, topk=(1, 5)
+            )
+            arch_losses.update(arch_loss.item(), arch_inputs.size(0))
+            arch_top1.update(arch_prec1.item(), arch_inputs.size(0))
+            arch_top5.update(arch_prec5.item(), arch_inputs.size(0))
 
-        if step % print_freq == 0 or step + 1 == len(xloader):
-            Sstr = (
-                "*SEARCH* "
-                + time_string()
-                + " [{:}][{:03d}/{:03d}]".format(epoch_str, step, len(xloader))
-            )
-            Tstr = "Time {batch_time.val:.2f} ({batch_time.avg:.2f}) Data {data_time.val:.2f} ({data_time.avg:.2f})".format(
-                batch_time=batch_time, data_time=data_time
-            )
-            Wstr = "Base [Loss {loss.val:.3f} ({loss.avg:.3f})  Prec@1 {top1.val:.2f} ({top1.avg:.2f}) Prec@5 {top5.val:.2f} ({top5.avg:.2f})]".format(
-                loss=base_losses, top1=base_top1, top5=base_top5
-            )
-            Astr = "Arch [Loss {loss.val:.3f} ({loss.avg:.3f})  Prec@1 {top1.val:.2f} ({top1.avg:.2f}) Prec@5 {top5.val:.2f} ({top5.avg:.2f})]".format(
-                loss=arch_losses, top1=arch_top1, top5=arch_top5
-            )
-            logger.log(Sstr + " " + Tstr + " " + Wstr + " " + Astr)
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if step % print_freq == 0 or step + 1 == len(xloader):
+                Sstr = (
+                    "*SEARCH* "
+                    + time_string()
+                    + " [{:}][{:03d}/{:03d}]".format(epoch_str, step, len(xloader))
+                )
+                Tstr = "Time {batch_time.val:.2f} ({batch_time.avg:.2f}) Data {data_time.val:.2f} ({data_time.avg:.2f})".format(
+                    batch_time=batch_time, data_time=data_time
+                )
+                Wstr = "Base [Loss {loss.val:.3f} ({loss.avg:.3f})  Prec@1 {top1.val:.2f} ({top1.avg:.2f}) Prec@5 {top5.val:.2f} ({top5.avg:.2f})]".format(
+                    loss=base_losses, top1=base_top1, top5=base_top5
+                )
+                Astr = "Arch [Loss {loss.val:.3f} ({loss.avg:.3f})  Prec@1 {top1.val:.2f} ({top1.avg:.2f}) Prec@5 {top5.val:.2f} ({top5.avg:.2f})]".format(
+                    loss=arch_losses, top1=arch_top1, top5=arch_top5
+                )
+                logger.log(Sstr + " " + Tstr + " " + Wstr + " " + Astr)
     return (
         base_losses.avg,
         base_top1.avg,
@@ -196,24 +199,31 @@ def main(xargs):
             },
             None,
         )
-    search_model = get_cell_based_tiny_net(model_config)
-    logger.log("search-model :\n{:}".format(search_model))
+
+    search_model = {}
+    w_optimizer = {}
+    a_optimizer = {}
+    w_scheduler = {}
+    a_scheduler = {}
+
+    search_globle_model = get_cell_based_tiny_net(model_config).cuda()
+    for one in search_loader:
+        search_model[one] = get_cell_based_tiny_net(model_config).cuda()
+        search_model[one].load_state_dict(search_globle_model.state_dict())
+        w_optimizer[one], w_scheduler[one], criterion = get_optim_scheduler(search_model[one].get_weights(), config)
+        a_optimizer[one] = torch.optim.Adam( search_model[one].get_alphas(), lr=xargs.arch_learning_rate, betas=(0.5, 0.999), weight_decay=xargs.arch_weight_decay,)
+
+
+    criterion = criterion.cuda()
+    logger.log("search-model :\n{:}".format(search_globle_model))
     logger.log("model-config : {:}".format(model_config))
 
-    w_optimizer, w_scheduler, criterion = get_optim_scheduler(
-        search_model.get_weights(), config
-    )
-    a_optimizer = torch.optim.Adam(
-        search_model.get_alphas(),
-        lr=xargs.arch_learning_rate,
-        betas=(0.5, 0.999),
-        weight_decay=xargs.arch_weight_decay,
-    )
-    logger.log("w-optimizer : {:}".format(w_optimizer))
-    logger.log("a-optimizer : {:}".format(a_optimizer))
-    logger.log("w-scheduler : {:}".format(w_scheduler))
-    logger.log("criterion   : {:}".format(criterion))
-    flop, param = get_model_infos(search_model, xshape)
+
+    # logger.log("w-optimizer : {:}".format(w_optimizer))
+    # logger.log("a-optimizer : {:}".format(a_optimizer))
+    # logger.log("w-scheduler : {:}".format(w_scheduler))
+    # logger.log("criterion   : {:}".format(criterion))
+    flop, param = get_model_infos(search_globle_model, xshape)
     logger.log("FLOP = {:.2f} M, Params = {:.2f} MB".format(flop, param))
     logger.log("search-space [{:} ops] : {:}".format(len(search_space), search_space))
     if xargs.arch_nas_dataset is None:
@@ -227,33 +237,32 @@ def main(xargs):
         logger.path("model"),
         logger.path("best"),
     )
-    network, criterion = torch.nn.DataParallel(search_model).cuda(), criterion.cuda()
 
-    if last_info.exists():  # automatically resume from previous checkpoint
-        logger.log(
-            "=> loading checkpoint of the last-info '{:}' start".format(last_info)
-        )
-        last_info = torch.load(last_info)
-        start_epoch = last_info["epoch"]
-        checkpoint = torch.load(last_info["last_checkpoint"])
-        genotypes = checkpoint["genotypes"]
-        valid_accuracies = checkpoint["valid_accuracies"]
-        search_model.load_state_dict(checkpoint["search_model"])
-        w_scheduler.load_state_dict(checkpoint["w_scheduler"])
-        w_optimizer.load_state_dict(checkpoint["w_optimizer"])
-        a_optimizer.load_state_dict(checkpoint["a_optimizer"])
-        logger.log(
-            "=> loading checkpoint of the last-info '{:}' start with {:}-th epoch.".format(
-                last_info, start_epoch
-            )
-        )
-    else:
-        logger.log("=> do not find the last-info file : {:}".format(last_info))
-        start_epoch, valid_accuracies, genotypes = (
-            0,
-            {"best": -1},
-            {-1: search_model.genotype()},
-        )
+    # if last_info.exists():  # automatically resume from previous checkpoint
+    #     logger.log(
+    #         "=> loading checkpoint of the last-info '{:}' start".format(last_info)
+    #     )
+    #     last_info = torch.load(last_info)
+    #     start_epoch = last_info["epoch"]
+    #     checkpoint = torch.load(last_info["last_checkpoint"])
+    #     genotypes = checkpoint["genotypes"]
+    #     valid_accuracies = checkpoint["valid_accuracies"]
+    #     search_model.load_state_dict(checkpoint["search_model"])
+    #     w_scheduler.load_state_dict(checkpoint["w_scheduler"])
+    #     w_optimizer.load_state_dict(checkpoint["w_optimizer"])
+    #     a_optimizer.load_state_dict(checkpoint["a_optimizer"])
+    #     logger.log(
+    #         "=> loading checkpoint of the last-info '{:}' start with {:}-th epoch.".format(
+    #             last_info, start_epoch
+    #         )
+    #     )
+    # else:
+    logger.log("=> do not find the last-info file : {:}".format(last_info))
+    start_epoch, valid_accuracies, genotypes = (
+        0,
+        {"best": -1},
+        {-1: search_globle_model.genotype()},
+    )
 
     # start training
     start_time, search_time, epoch_time, total_epoch = (
@@ -263,19 +272,23 @@ def main(xargs):
         config.epochs + config.warmup,
     )
     for epoch in range(start_epoch, total_epoch):
-        w_scheduler.update(epoch, 0.0)
-        need_time = "Time Left: {:}".format(
-            convert_secs2time(epoch_time.val * (total_epoch - epoch), True)
-        )
-        epoch_str = "{:03d}-{:03d}".format(epoch, total_epoch)
-        search_model.set_tau(
-            xargs.tau_max - (xargs.tau_max - xargs.tau_min) * epoch / (total_epoch - 1)
-        )
-        logger.log(
-            "\n[Search the {:}-th epoch] {:}, tau={:}, LR={:}".format(
-                epoch_str, need_time, search_model.get_tau(), min(w_scheduler.get_lr())
+
+        for user in w_scheduler:
+            w_scheduler[user].update(epoch, 0.0)
+            search_model[user].set_tau(
+                xargs.tau_max - (xargs.tau_max - xargs.tau_min) * epoch / (total_epoch - 1)
             )
-        )
+
+        # need_time = "Time Left: {:}".format(
+        #     convert_secs2time(epoch_time.val * (total_epoch - epoch), True)
+        # )
+        epoch_str = "{:03d}-{:03d}".format(epoch, total_epoch)
+
+        # logger.log(
+        #     "\n[Search the {:}-th epoch] {:}, tau={:}, LR={:}".format(
+        #         epoch_str, need_time, search_model.get_tau(), min(w_scheduler.get_lr())
+        #     )
+        # )
         weight_list = []
         acc_list = []
         for user in search_loader:
@@ -288,11 +301,11 @@ def main(xargs):
                 weight
             ) = search_func(
                 search_loader[user],
-                copy.deepcopy(network),
+                search_model[user],
                 criterion,
-                w_scheduler,
-                w_optimizer,
-                a_optimizer,
+                w_scheduler[user],
+                w_optimizer[user],
+                a_optimizer[user],
                 epoch_str,
                 xargs.print_freq,
                 logger,
@@ -315,7 +328,10 @@ def main(xargs):
 
         global_weights = average_weights(weight_list)
 
-        network.load_state_dict(global_weights)
+        search_globle_model.load_state_dict(global_weights)
+
+        for user in search_model:
+            search_model[user].load_state_dict(global_weights)
 
         search_time.update(time.time() - start_time)
 
@@ -328,34 +344,34 @@ def main(xargs):
         # else:
         #     find_best = False
 
-        genotypes[epoch] = search_model.genotype()
+        genotypes[epoch] = search_globle_model.genotype()
         logger.log(
             "<<<--->>> The {:}-th epoch : {:}".format(epoch_str, genotypes[epoch])
         )
         # save checkpoint
-        save_path = save_checkpoint(
-            {
-                "epoch": epoch + 1,
-                "args": deepcopy(xargs),
-                "search_model": search_model.state_dict(),
-                "w_optimizer": w_optimizer.state_dict(),
-                "a_optimizer": a_optimizer.state_dict(),
-                "w_scheduler": w_scheduler.state_dict(),
-                "genotypes": genotypes,
-                "valid_accuracies": valid_accuracies,
-            },
-            model_base_path,
-            logger,
-        )
-        last_info = save_checkpoint(
-            {
-                "epoch": epoch + 1,
-                "args": deepcopy(args),
-                "last_checkpoint": save_path,
-            },
-            logger.path("info"),
-            logger,
-        )
+        # save_path = save_checkpoint(
+        #     {
+        #         "epoch": epoch + 1,
+        #         "args": deepcopy(xargs),
+        #         "search_model": search_model.state_dict(),
+        #         "w_optimizer": w_optimizer.state_dict(),
+        #         "a_optimizer": a_optimizer.state_dict(),
+        #         "w_scheduler": w_scheduler.state_dict(),
+        #         "genotypes": genotypes,
+        #         "valid_accuracies": valid_accuracies,
+        #     },
+        #     model_base_path,
+        #     logger,
+        # )
+        # last_info = save_checkpoint(
+        #     {
+        #         "epoch": epoch + 1,
+        #         "args": deepcopy(args),
+        #         "last_checkpoint": save_path,
+        #     },
+        #     logger.path("info"),
+        #     logger,
+        # )
         # if find_best:
         #     logger.log(
         #         "<<<--->>> The {:}-th epoch : find the highest validation accuracy : {:.2f}%.".format(
@@ -364,7 +380,7 @@ def main(xargs):
         #     )
         #     copy_checkpoint(model_base_path, model_best_path, logger)
         with torch.no_grad():
-            logger.log("{:}".format(search_model.show_alphas()))
+            logger.log("{:}".format(search_globle_model.show_alphas()))
         if api is not None:
             logger.log("{:}".format(api.query_by_arch(genotypes[epoch], "200")))
         # measure elapsed time
